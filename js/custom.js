da0e70c8f253c7f3ae21c3b996ace24244249561
starloox <starlooxglobal.official@gmail.com>
2025-07-28 04:21:06 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,496 @@
+
+
+p_product={
+
+'pant':[{'img':"images/product/pant/pant-01.jpg",
+         'price':4000,
+         'name':"white shirt"},
+         
+         {'img':"images/product/pant/pant-02.jpg",
+         'price':7000,
+         'name':"blue fabric"},
+         
+         {'img':"images/product/pant/pant-03.jpg",
+         'price':8000,
+         'name':"glowing fab"},
+         
+         {'img':"images/product/pant/pant-04.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 {'img':"images/product/pant/pant-05.jpg",
+         'price':4000,
+         'name':"white shirt"},
+         
+         {'img':"images/product/pant/pant-06.jpg",
+         'price':7000,
+         'name':"blue fabric"},
+         
+         {'img':"images/product/pant/pant-07.jpg",
+         'price':8000,
+         'name':"glowing fab"},
+         
+         {'img':"images/product/pant/pant-08.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		  {'img':"images/product/pant/pant-09.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 { 'img':"images/product/pant/pant-10.jpg",
+         'price':4000,
+         'name':"white shirt"},
+         
+         {'img':"images/product/pant/pant-11.jpg",
+         'price':7000,
+         'name':"blue fabric"},
+         
+         {'img':"images/product/pant/pant-12.jpg",
+         'price':8000,
+         'name':"glowing fab"},
+         
+         {'img':"images/product/pant/pant-13.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+        
+         
+         
+         
+         
+         ],
+         
+         
+'shirt':[{'img':"images/product/shirt/shirt-01.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-02.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-03.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-04.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-05.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-06.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 {'img':"images/product/shirt/shirt-07.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-08.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-09.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-10.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-11.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/shirt/shirt-12.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         
+         ],
+         
+         
+         
+ 'frock':[{'img':"images/product/frock/frock-01.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-02.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-03.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-04.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+         {'img':"images/product/frock/frock-05.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-06.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-07.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-08.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 {'img':"images/product/frock/frock-09.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-10.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-11.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-12.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+         {'img':"images/product/frock/frock-13.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-14.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-15.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-16.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 {'img':"images/product/frock/frock-17.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-18.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+         {'img':"images/product/frock/frock-19.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-20.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-21.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/frock/frock-22.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         
+         ],
+         
+         
+'babasuit':[{'img':"images/product/babasuit/babasuit-01.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-02.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-03.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-04.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-05.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-06.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 {'img':"images/product/babasuit/babasuit-07.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-08.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-09.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-10.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-11.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-12.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+		 {'img':"images/product/babasuit/babasuit-13.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-14.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-15.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-16.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-17.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-18.jpg",
+         'price':9000,
+         'name':"golden color"},
+		 
+		 {'img':"images/product/babasuit/babasuit-19.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-20.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-21.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-22.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-23.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         {'img':"images/product/babasuit/babasuit-24.jpg",
+         'price':9000,
+         'name':"golden color"},
+         
+         ],
+         
+         
+         
+         
+         
+ 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const queryString = window.location.search;
+		    const urlParams = new URLSearchParams(queryString);
+		    const item = urlParams.get('item');
+		    
+		    //alert("urlv"+item);
+		    var brk=item;
+
+
+
+
+var opt=1;
+var cat;
+*/
+
+/*
+
+localStorage.setItem('catList',kl);
+localStorage.setItem('itemList',itemList);
+       var itemList = localStorage.getItem('itemList');
+	   
+   //alert("itemlist"+itemList);
+
+
+
+
+
+*/
+
+
+ 
+
+
+
+//localStorage.setItem('catList',"rajibul");
+
+
+function show_list(ct,d){
+
+
+		var cat=ct;
+var r=document.getElementById(d);	
+var key=Object.keys(p_product);	
+var kl=key.length;	
+//alert("catagoryList"+kl);
+//alert('key'+key[0]);
+
+   
+   for(var k=0;k<kl;k++){
+   // alert("catagory"+k);
+     var itemList=p_product[key[k]].length;
+   //alert(itemList);
+
+       
+  
+    
+     
+
+
+
+		for(i=0;i<itemList;i++){
+	 
+	  //  alert("item"+i);
+	  
+	   
+		document.getElementById(d).innerHTML+=`
+		    <div  class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item ${key[k]}">
+		    
+		        <div class="block2">
+		        <div class="block2-pic hov-img0">
+		       <img src='${p_product[key[k]][i]['img']}' alt="IMG-PRODUCT">
+		       <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+		       Quick View
+		       </a>
+		       </div>
+		       </div>
+		       <div class="block2-txt flex-w flex-t p-t-14">
+		       <div class="block2-txt-child1 flex-col-l ">
+		       <a href="product-detail.html" class="stexmt-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+		      <!--
+		       ${p_product[key[k]][i]['name']}
+			   -->
+		       </a>
+		       
+		       <span class="stext-105 cl3">
+			   <!--
+		       ${p_product[key[k]][i]['price']}
+			   -->
+		       </span>
+		       </div>
+		       <!--
+		       <div class="block2-txt-child2 flex-r p-t-3">
+		       <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+		       <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+		       <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+		       </a>
+		       </div>
+			   -->
+		       
+		      </div>
+		       
+		   
+		       
+		       
+		       
+		       
+		       `;
+		       
+		       
+		     
+		    
+		       
+		       }
+
+           
+		     
+		  
+		    
+
+
+}
+
+}
+
+
+	
+sessionStorage.setItem('username', 'JohnDoe');
+sessionStorage.setItem('theme', 'dark');
+//const catList = localStorage.getItem('catList');
+//const itemList = localStorage.getItem('itemList');
+//alert("catlist"+catList); // Output: JohnDoe
+//alert("itemlist"+itemList);
+
+
+
+
+
